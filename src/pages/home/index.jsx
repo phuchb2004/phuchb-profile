f0cd3aa8d1abb56beac83a9495f59e0df6b1720a
PHUC\phuchb <phuchb04@gmail.com>
2025-12-18 15:39:18 +0700

    add animation for homepage

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -8,23 +8,33 @@ export default function Home() {
     return (
         <div className="home-container">
             <div className="home-left">
-                <h1>{t('home.left.greeting')}</h1>
-                <p>
-                    <Trans
-                        i18nKey="home.left.description"
-                        components={{
-                            0: <strong className="highlight-text" />,
-                        }}
-                    />
-                </p>
-                <div className="buttons">
+                <h1 className="animate-fade-up">
+                    {t('home.left.greeting')}
+                </h1>
+                
+                <div className="animate-fade-up delay-1">
+                    <p>
+                        <Trans
+                            i18nKey="home.left.description"
+                            components={{
+                                0: <strong className="highlight-text" />,
+                            }}
+                        />
+                    </p>
+                </div>
+
+                <div className="buttons animate-fade-up delay-2">
                     <button className="button-contact">{t('home.left.contactButton')}</button>
                     <button className="button-cv">{t('home.left.downloadCVButton')}</button>
                 </div>
             </div>
 
             <div className="home-right">
-                <img src={phucImg} alt="Phuc" />
+                <img 
+                    src={phucImg}
+                    alt="Phuc"
+                    className="animate-fade-in delay-3"
+                />
             </div>
         </div>
     )
