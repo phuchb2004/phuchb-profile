778069fad37af17e1d9e4d755e7289c509ac5491
PHUC\phuchb <phuchb04@gmail.com>
2025-12-18 13:54:58 +0700

    create homepage content

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,22 @@
 import './style.css';
+import phucImg from '../../assets/sen.png';
 
 export default function Home() {
 
     return (
         <div className="home-container">
-            <h1>Welcome to My Profile</h1> 
+            <div className="home-left">
+                <h1>Xin chào, tôi là Bảo Phúc</h1>
+                <p>Một Full-stack Developer đam mê tạo ra những giao diện hiện đại bằng <strong>React, Next.js, Vue.js</strong> và xây dựng hệ thống ổn định với <strong>Node.js, Express, Nest.js</strong></p>
+                <div className="buttons">
+                    <button className="button-contact">Liên hệ tôi</button>
+                    <button className="button-cv">Tải xuống CV</button>
+                </div>
+            </div>
+
+            <div className="home-right">
+                <img src={phucImg} alt="Phuc" />
+            </div>
         </div>
     )
 }
